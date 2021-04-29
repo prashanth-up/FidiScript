a8a60587827fef6d93d3a88f6b4c18f75453c627
Prashanth <ujprashant@gmail.com>
2021-04-29 20:42:01 +0530

    Added Error Classes and Tracebacks to error. FML

--- litpy.py
+++ litpy.py
@@ -40,8 +40,34 @@ class InvalidSyntaxError(Error):
     def __init__(self, pos_start, pos_end, details=''):
         super().__init__(pos_start, pos_end, 'Invalid Syntax', details)
 
+
+class RTError(Error):
+    def __init__(self, pos_start, pos_end, details, context):
+        super().__init__(pos_start, pos_end, 'Runtime Error', details)
+        self.context = context
+
+    def as_string(self):
+        result = self.generate_traceback()
+        result += f'{self.error_name}: {self.details}'
+        result += '\n\n' + \
+            string_with_arrows(self.pos_start.ftxt,
+                               self.pos_start, self.pos_end)
+        return result
+
+    def generate_traceback(self):
+        result = ''
+        pos = self.pos_start
+        ctx = self.context
+
+        while ctx:
+            result = f'  File {pos.fn}, line {str(pos.ln + 1)}, in {ctx.display_name}\n' + result
+            pos = ctx.parent_entry_pos
+            ctx = ctx.parent
+
+        return 'Traceback (most recent call last):\n' + result
+
 # ----------------------------------------
-# POSITION TICKER
+# POSITION POINTER
 # ----------------------------------------
 
 
@@ -67,7 +93,7 @@ class Position:
         return Position(self.idx, self.ln, self.col, self.fn, self.ftxt)
 
 # ----------------------------------------
-# TOKEN CLASS
+# TOKENS
 # ----------------------------------------
 
 
@@ -182,6 +208,9 @@ class NumberNode:
     def __init__(self, tok):
         self.tok = tok
 
+        self.pos_start = self.tok.pos_start
+        self.pos_end = self.tok.pos_end
+
     def __repr__(self):
         return f'{self.tok}'
 
@@ -192,6 +221,9 @@ class BinOpNode:
         self.op_tok = op_tok
         self.right_node = right_node
 
+        self.pos_start = self.left_node.pos_start
+        self.pos_end = self.right_node.pos_end
+
     def __repr__(self):
         return f'({self.left_node}, {self.op_tok}, {self.right_node})'
 
@@ -201,11 +233,14 @@ class UnaryOpNode:
         self.op_tok = op_tok
         self.node = node
 
+        self.pos_start = self.op_tok.pos_start
+        self.pos_end = node.pos_end
+
     def __repr__(self):
         return f'({self.op_tok}, {self.node})'
 
 # ----------------------------------------
-# PARSE RESULT CLASS
+# PARSE RESULT
 # ----------------------------------------
 
 
@@ -256,8 +291,6 @@ class Parser:
             ))
         return res
 
-# ----------------------------------------
-
     def factor(self):
         res = ParseResult()
         tok = self.current_tok
@@ -298,8 +331,6 @@ class Parser:
     def expr(self):
         return self.bin_op(self.term, (TT_PLUS, TT_MINUS))
 
-# ----------------------------------------
-
     def bin_op(self, func, ops):
         res = ParseResult()
         left = res.register(func())
@@ -316,67 +347,144 @@ class Parser:
 
         return res.success(left)
 
+# ----------------------------------------
+# RUNTIME RESULT
+# ----------------------------------------
+
+
+class RTResult:
+    def __init__(self):
+        self.value = None
+        self.error = None
+
+    def register(self, res):
+        if res.error:
+            self.error = res.error
+        return res.value
+
+    def success(self, value):
+        self.value = value
+        return self
+
+    def failure(self, error):
+        self.error = error
+        return self
 
 # ----------------------------------------
 # VALUES
 # ----------------------------------------
 
+
 class Number:
     def __init__(self, value):
         self.value = value
         self.set_pos()
+        self.set_context()
 
     def set_pos(self, pos_start=None, pos_end=None):
         self.pos_start = pos_start
         self.pos_end = pos_end
         return self
 
+    def set_context(self, context=None):
+        self.context = context
+        return self
+
     def added_to(self, other):
         if isinstance(other, Number):
-            return Number(self.value + other.value)
+            return Number(self.value + other.value).set_context(self.context), None
 
-    def subbed_to(self, other):
+    def subbed_by(self, other):
         if isinstance(other, Number):
-            return Number(self.value - other.value)
+            return Number(self.value - other.value).set_context(self.context), None
 
-    def multed_to(self, other):
+    def multed_by(self, other):
         if isinstance(other, Number):
-            return Number(self.value * other.value)
+            return Number(self.value * other.value).set_context(self.context), None
 
-    def dived_to(self, other):
+    def dived_by(self, other):
         if isinstance(other, Number):
-            return Number(self.value / other.value)
+            if other.value == 0:
+                return None, RTError(
+                    other.pos_start, other.pos_end,
+                    'Division by zero',
+                    self.context
+                )
+
+            return Number(self.value / other.value).set_context(self.context), None
 
     def __repr__(self):
         return str(self.value)
 
+# ----------------------------------------
+# CONTEXT
+# ----------------------------------------
+
+
+class Context:
+    def __init__(self, display_name, parent=None, parent_entry_pos=None):
+        self.display_name = display_name
+        self.parent = parent
+        self.parent_entry_pos = parent_entry_pos
 
 # ----------------------------------------
-# Interpreter
+# INTERPRETER
 # ----------------------------------------
 
 
 class Interpreter:
-    def visit(self, node):
+    def visit(self, node, context):
         method_name = f'visit_{type(node).__name__}'
         method = getattr(self, method_name, self.no_visit_method)
-        return method(node)
+        return method(node, context)
 
-    def no_visit_method(self, node):
-        raise Exception(
-            f'No visit_{type(node).__name__} method defined bruh, check again dumbass')
+    def no_visit_method(self, node, context):
+        raise Exception(f'No visit_{type(node).__name__} method defined')
 
-    def visit_NumberNode(self, node):
-        print("Found the number node! woohoo!")
+    def visit_NumberNode(self, node, context):
+        return RTResult().success(
+            Number(node.tok.value).set_context(
+                context).set_pos(node.pos_start, node.pos_end)
+        )
 
-    def visit_BinOpNode(self, node):
-        print("Found bin op node!!")
-        self.visit(node.left_node)
-        self.visit(node.right_node)
+    def visit_BinOpNode(self, node, context):
+        res = RTResult()
+        left = res.register(self.visit(node.left_node, context))
+        if res.error:
+            return res
+        right = res.register(self.visit(node.right_node, context))
+        if res.error:
+            return res
 
-    def visit_UnaryOpNode(self, node):
-        print("Found unary op node! urinary* lmao")
-        self.visit(node.node)
+        if node.op_tok.type == TT_PLUS:
+            result, error = left.added_to(right)
+        elif node.op_tok.type == TT_MINUS:
+            result, error = left.subbed_by(right)
+        elif node.op_tok.type == TT_MUL:
+            result, error = left.multed_by(right)
+        elif node.op_tok.type == TT_DIV:
+            result, error = left.dived_by(right)
+
+        if error:
+            return res.failure(error)
+        else:
+            return res.success(result.set_pos(node.pos_start, node.pos_end))
+
+    def visit_UnaryOpNode(self, node, context):
+        res = RTResult()
+        number = res.register(self.visit(node.node, context))
+        if res.error:
+            return res
+
+        error = None
+
+        if node.op_tok.type == TT_MINUS:
+            number, error = number.multed_by(Number(-1))
+
+        if error:
+            return res.failure(error)
+        else:
+            return res.success(number.set_pos(node.pos_start, node.pos_end))
 
 # ----------------------------------------
 # RUN
@@ -390,13 +498,15 @@ def run(fn, text):
     if error:
         return None, error
 
-    # Generate Abstract Syntax Tree
+    # Generate AST
     parser = Parser(tokens)
     ast = parser.parse()
     if ast.error:
         return None, ast.error
 
+    # Run program
     interpreter = Interpreter()
-    interpreter.visit(ast.node)
+    context = Context('<program>')
+    result = interpreter.visit(ast.node, context)
 
-    return None, None
+    return result.value, result.error
