08fc3933f851ee70659bc409807edecbb44e7c47
Prashanth <ujprashant@gmail.com>
2021-05-05 18:46:15 +0530

    Grammar rules and error handling for char added

--- litpy.py
+++ litpy.py
@@ -44,6 +44,11 @@ class InvalidSyntaxError(Error):
         super().__init__(pos_start, pos_end, 'Invalid Syntax', details)
 
 
+class ExpectedCharError(Error):
+	def __init__(self, pos_start, pos_end, details):
+		super().__init__(pos_start, pos_end, 'Expected Character', details)
+
+
 class RTError(Error):
     def __init__(self, pos_start, pos_end, details, context):
         super().__init__(pos_start, pos_end, 'Runtime Error', details)
