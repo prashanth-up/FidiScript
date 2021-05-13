1cdd5cadb83f6790fb34fd7bc36491f3876eaeee
Prashanth <ujprashant@gmail.com>
2021-05-13 23:11:15 +0530

    Added String Class and basic string capabilities

--- litpy.py
+++ litpy.py
@@ -1251,6 +1251,37 @@ class Number(Value):
     def __repr__(self):
         return str(self.value)
 
+
+class String(Value):
+    def __init__(self, value):
+        super().__init__()
+        self.value = value
+
+    def added_to(self, other):
+        if isinstance(other, String):
+            return String(self.value + other.value).set_context(self.context), None
+        else:
+            return None, Value.illegal_operation(self, other)
+
+    def multed_by(self, other):
+        if isinstance(other, Number):
+            return String(self.value * other.value).set_context(self.context), None
+        else:
+            return None, Value.illegal_operation(self, other)
+
+    def is_true(self):
+        return len(self.value) > 0
+
+    def copy(self):
+        copy = String(self.value)
+        copy.set_pos(self.pos_start, self.pos_end)
+        copy.set_context(self.context)
+        return copy
+
+    def __repr__(self):
+        return f'"{self.value}"'
+
+
 class Function(Value):
     def __init__(self, name, body_node, arg_names):
         super().__init__()
